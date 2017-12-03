2a113ec35a2267bf731ef25a39e29d0b7acf1049
wuggy <github@wuggy.org>
2017-12-03 12:14:06 +0000

    Fixed erroneous refs to Day1 in Day tests

--- day02/testday02.cpp
+++ day02/testday02.cpp
@@ -7,7 +7,7 @@
 #include "day02.h"
 #include "../utils/aocutils.h"
 
-TEST(DAY01, Part1Case1)
+TEST(DAY02, Part1Case1)
 {
   std::istringstream ss("5 1 9 5\n7 5 3\n2 4 6 8\n");
   std::vector< std::vector<std::size_t> > sheet = AOCUtils::parseByLines<std::vector<std::size_t> >(ss, AOCUtils::parseLine<std::size_t>);
@@ -16,7 +16,7 @@ TEST(DAY01, Part1Case1)
   ASSERT_EQ(actual, 18);
 }
 
-TEST(DAY01, Part2Case1)
+TEST(DAY02, Part2Case1)
 {
   std::istringstream ss("5 9 2 8\n9 4 7 3\n3 8 6 5\n");
   std::vector< std::vector<std::size_t> > sheet = AOCUtils::parseByLines<std::vector<std::size_t> >(ss, AOCUtils::parseLine<std::size_t>);
